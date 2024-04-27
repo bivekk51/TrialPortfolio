1a6a381df0c2c5242ed7f9ce3d4c2468b706e2b0
Bivek Nepal <115235822+nepaldynasty09@users.noreply.github.com>
2024-04-27 15:38:09 +0000

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Bivek Nepal",
+  title: "Software Engineer",
+  email: "nepalbivek08@gmail.com",
+  gitHub: "nepaldynasty09",
+  instagram: "npc_51",
+  linkedIn: "",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
